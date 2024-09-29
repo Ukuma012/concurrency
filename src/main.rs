6085f534332b6f72444d30ad04c4f9c19b07e137
Ukuma012 <ukuma012@gmail.com>
2024-09-29 12:58:18 +0900

    Mutex

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
-use std::thread::spawn;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
-fn hello() {
-    println!("Hello World");
-}
-
-fn my_func12() {
-    let v = 10;
-    let f = move || v * 2;
-
-    let result = spawn(f).join();
-    println!("result = {:?}", result);
-
-    match spawn(|| panic!("I'm panicked!")).join() {
-        Ok(_) => {
-            println!("successed");
-        }
-        Err(a) => {
-            let s = a.downcast_ref::<&str>();
-            println!("failed: {:?}", s);
-        }
+fn some_func(lock: Arc<Mutex<u64>>) {
+    loop {
+        // ロックしないとMutex型の中の値は参照不可
+        let mut val = lock.lock().unwrap();
+        *val += 1;
+        println!("{}", val);
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let lock0 = Arc::new(Mutex::new(0));
+
+    let lock1 = lock0.clone();
+
+    let th0 = thread::spawn(move || {
+        some_func(lock0);
+    });
+
+    let th1 = thread::spawn(move || {
+        some_func(lock1);
+    });
+
+    th0.join().unwrap();
+    th1.join().unwrap();
 }
