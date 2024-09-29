e027507300b77526c000ee8810c58a1673cb39a8
Ukuma012 <ukuma012@gmail.com>
2024-09-29 13:04:43 +0900

    RWLock

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,17 @@
-use std::sync::{Arc, Mutex};
-use std::thread;
-
-fn some_func(lock: Arc<Mutex<u64>>) {
-    loop {
-        // ロックしないとMutex型の中の値は参照不可
-        let mut val = lock.lock().unwrap();
-        *val += 1;
-        println!("{}", val);
-    }
-}
+use std::sync::RwLock;
 
 fn main() {
-    let lock0 = Arc::new(Mutex::new(0));
+    let lock = RwLock::new(10);
+    {
+        let v1 = lock.read().unwrap();
+        let v2 = lock.read().unwrap();
+        println!("v1 = {}", v1);
+        println!("v2 = {}", v2);
+    }
 
-    let lock1 = lock0.clone();
-
-    let th0 = thread::spawn(move || {
-        some_func(lock0);
-    });
-
-    let th1 = thread::spawn(move || {
-        some_func(lock1);
-    });
-
-    th0.join().unwrap();
-    th1.join().unwrap();
+    {
+        let mut v = lock.write().unwrap();
+        *v = 7;
+        println!("v = {}", v);
+    }
 }
